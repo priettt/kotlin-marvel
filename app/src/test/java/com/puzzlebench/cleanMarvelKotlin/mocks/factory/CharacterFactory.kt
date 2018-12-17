cf42e43086ab1cb1b6e67f1d0c8d3047beb74cf5
Francisco Prieto <francisco.prieto@globant.com>
2018-12-17 16:23:34 -0300

    Downgraded Picasso and fixed unit tests

--- app/src/test/java/com/puzzlebench/cleanMarvelKotlin/mocks/factory/CharacterFactory.kt
+++ app/src/test/java/com/puzzlebench/cleanMarvelKotlin/mocks/factory/CharacterFactory.kt
@@ -1,12 +1,18 @@
 package com.puzzlebench.cleanMarvelKotlin.mocks.factory
 
 import com.puzzlebench.cleanMarvelKotlin.domain.model.Character
+import com.puzzlebench.cleanMarvelKotlin.domain.model.Comics
 import com.puzzlebench.cleanMarvelKotlin.domain.model.Thumbnail
+import com.puzzlebench.cleanMarvelKotlin.domain.model.Url
 
 
 class CharactersFactory {
 
     companion object Factory {
+        private const val BASE_ID = 1
+        private const val BASE_AVAILABLE = 1
+        private const val BASE_LINK = "www.marvel.com"
+        private const val BASE_TYPE = "comiclink"
         private const val BASE_NAME = "Name"
         private const val BASE_DESCRIPTION = "Description"
         private const val BASE_PATH = "image"
@@ -14,7 +20,14 @@ class CharactersFactory {
 
         open fun getMockCharacter(): List<Character> {
             return listOf(1..5).map {
-                Character("$BASE_NAME$it", "$BASE_DESCRIPTION$it", Thumbnail("$BASE_PATH$it", BASE_EXTENSION))
+                Character(
+                        it.step,
+                        "$BASE_NAME$it",
+                        "$BASE_DESCRIPTION$it",
+                        Thumbnail("$BASE_PATH$it", BASE_EXTENSION),
+                        Comics(BASE_AVAILABLE),
+                        listOf(Url(BASE_TYPE, BASE_LINK))
+                )
             }
         }
     }
